489156afb640b5557e4b6940994ed31e7337218c
idobry <idobry@hotmail.co.il>
2016-07-22 18:12:30 +0300

    Add files via upload

---
+++ MultiDictionary.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp
+{
+    public class MultiDictionary<K, V> : IMultiDictionary<K, V>, IEnumerable<KeyValuePair<K, IEnumerable<V>>> where K : struct where V : new()
+    {
+        public Dictionary<K, LinkedList<V>> Dictionary { get; }
+
+        #region Properties
+        public ICollection<K> Keys { get; }
+        public ICollection<V> Values { get; }
+        public int Count { get; private set; }
+        #endregion
+
+        public MultiDictionary()
+        {
+            Dictionary = new Dictionary<K, LinkedList<V>>();
+            Count = 0;
+        }
+        public void Add(K key, V value)
+        {
+            if (typeof(K).GetCustomAttributes(typeof(KeyAttribute), true) == null)
+            {
+                throw new Exception("Must have an attribute");
+            }
+            else if (Dictionary.ContainsKey(key) == true)
+            {
+                Dictionary[key].AddLast(value);
+            }
+            else
+            {
+                Dictionary.Add(key, new LinkedList<V>());
+                Dictionary[key].AddLast(value);
+            }
+            Count++;
+        }
+
+        public void CreateNewValue(K key)
+        {
+            Add(key, new V());
+        }
+
+        public bool Remove(K key)
+        {
+            int i = Dictionary[key].Count;
+
+            if (!Dictionary.Remove(key))
+            {
+                return false;
+            }
+            Count -= i;
+
+            return true;
+        }
+
+        public bool Remove(K key, V value)
+        {
+            if (Dictionary.ContainsKey(key) == false)
+            {
+                return false;
+            }
+            if (Dictionary[key].Remove(value) == false)
+            {
+                return false;
+            }
+            Count--;
+
+            return true;
+        }
+
+        public void Clear()
+        {
+            Dictionary.Clear();
+            Count = 0;
+        }
+
+        public bool ContainsKey(K key)
+        {
+            var result = Dictionary.ContainsKey(key);
+            return result;
+        }
+
+        public bool Contains(K key, V value)
+        {
+            if (Dictionary.ContainsKey(key) == false)
+            {
+                return false;
+            }
+            var res = Dictionary[key].Contains(value);
+
+            return res;
+        }
+
+        public IEnumerator<KeyValuePair<K, IEnumerable<V>>> GetEnumerator()
+        {
+            var list = Dictionary.Select(item => new KeyValuePair<K, IEnumerable<V>>(item.Key, item.Value)).ToList();
+
+            return list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class KeyAttribute : Attribute
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericApp
+{
+    class Program
+    {
+        [Key]
+        public struct myKey
+        {
+            public int num;
+        }//Key
+
+        public struct myString
+        {
+            public string str;
+
+            public override string ToString()
+            {
+                return str;
+            }
+        }//myString
+
+        private static void Main(string[] args)
+        {
+            var multiDictionary = new MultiDictionary<myKey, myString>();
+
+            myKey number;
+            number.num = 1;
+            myString myStr;
+            myStr.str = "One";
+
+            myKey number2;
+            number2.num = 1;
+            myString myStr2;
+            myStr2.str = "ich";
+
+            multiDictionary.Add(number, myStr);
+            multiDictionary.Add(number2, myStr2);
+
+            DisplayDictionary(multiDictionary);
+            multiDictionary.Remove(number);
+            DisplayDictionary(multiDictionary);
+        }
+
+        private static void DisplayDictionary(MultiDictionary<myKey, myString> multiDictionary)
+        {
+            if (multiDictionary.Count != 0)
+            {
+                foreach (var key in multiDictionary)
+                {
+                    foreach (var value in key.Value)
+                    {
+                        Console.WriteLine($"Key: {key.Key.num}, Value: {value}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dictionary Empty");
+            }
+        }
+    }
+}
+
